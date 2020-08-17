ebe95c8b9f76737d72c763976b9705b73fb073ef
mtlquakers <mtlquakers@gmail.com>
2020-08-17 14:29:34 -0400

    index redirect update

--- assets/js/lang-choose.js
+++ assets/js/lang-choose.js
@@ -1,17 +1,7 @@
 ---
 ---
-window.onload = function() {
-  let choice = localStorage.getItem('lang');
-  if (choice === null) {
-	  console.log("undef");
-  }
-	else {
-          {% for each in site.data.navigation.welcome %} if (choice === "{{ each.lang }}") {window.location.replace("{{ each.link }}");}{% endfor %}
-	};
-}
-	
 function chooseLang(lang) {
-  let choice = lang;
-  localStorage.setItem('lang',choice);
-  {% for each in site.data.navigation.welcome %} if (choice === "{{ each.lang }}") {window.location.replace("{{ each.link }}");}{% endfor %}
+    let choice = lang;
+    localStorage.setItem('lang',choice);
+    {% for each in site.data.navigation.welcome %} if (choice === "{{ each.lang }}") {window.location.replace("{{ each.link }}");}{% endfor %}
 }
